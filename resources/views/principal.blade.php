abdc0036070eefd201fb06d2813ddd5f99d8b87d
Armas <luis.armas@cceo.com.mx>
2019-02-17 22:09:30 -0600

    Todo hasta cerrar sesión

--- resources/views/principal.blade.php
+++ resources/views/principal.blade.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
 <html lang="es">
-
+ 
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -13,9 +13,8 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <!-- Icons -->
     <link href="css/plantilla.css" rel="stylesheet">
-    
 </head>
-
+ 
 <body class="app header-fixed sidebar-fixed aside-menu-fixed aside-menu-hidden">
     <div id="app">
     <header class="app-header navbar">
@@ -57,36 +56,51 @@
             <li class="nav-item dropdown">
                 <a class="nav-link dropdown-toggle nav-link" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">
                     <img src="img/avatars/6.jpg" class="img-avatar" alt="admin@bootstrapmaster.com">
-                    <span class="d-md-down-none">admin </span>
+                    <span class="d-md-down-none">{{Auth::user()->usuario}} </span>
                 </a>
                 <div class="dropdown-menu dropdown-menu-right">
                     <div class="dropdown-header text-center">
                         <strong>Cuenta</strong>
                     </div>
-                    <a class="dropdown-item" href="#"><i class="fa fa-user"></i> Perfil</a>
-                    <a class="dropdown-item" href="#"><i class="fa fa-lock"></i> Cerrar sesión</a>
+                    <a class="dropdown-item" href="{{ route('logout') }}"
+                    onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                    <i class="fa fa-lock"></i> Cerrar sesión</a>
+ 
+                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                        {{ csrf_field() }}
+                    </form>
                 </div>
             </li>
         </ul>
     </header>
-
+ 
     <div class="app-body">
-        @include('plantilla.sidebar')
-
+         
+        @if(Auth::check())
+            @if (Auth::user()->idrol == 1)
+                @include('plantilla.sidebaradministrador')
+            @elseif (Auth::user()->idrol == 2)
+                @include('plantilla.sidebarvendedor')
+            @elseif (Auth::user()->idrol == 3)
+                @include('plantilla.sidebaralmacenero')
+            @else
+ 
+            @endif
+ 
+        @endif
         <!-- Contenido Principal -->
         @yield('contenido')
         <!-- /Fin del contenido principal -->
+    </div>   
     </div>
-
-    </div>
-
     <footer class="app-footer">
         <span><a href="http://www.incanatoit.com/">IncanatoIT</a> &copy; 2017</span>
         <span class="ml-auto">Desarrollado por <a href="http://www.incanatoit.com/">IncanatoIT</a></span>
     </footer>
-
+     
+ 
     <script src="js/app.js"></script>
     <script src="js/plantilla.js"></script>
 </body>
-
+ 
 </html>
